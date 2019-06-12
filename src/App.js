ef98eab042890efc247d5169c6badbcb342e497a
Leo Zheng <ting5zheng@gmail.com>
2019-06-12 17:22:57 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      classes: [
+        { name: "Mage", votes: 0 },
+        { name: "Archer", votes: 0 },
+        { name: "Priest", votes: 0 },
+        { name: "Warrior", votes: 0 },
+        { name: "Paladin", votes: 0 },
+        { name: "Assassin", votes: 0 },
+        { name: "Barbarian", votes: 0 },
+      ]
+    };
+  }
+
+  vote = (i) => {
+    console.log("Voting has occured", i);
+    let c = [...this.state.classes];
+    c[i].votes++;
+    this.setState({classes: c});
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Vote for Characters!</h1>
+        <div className="content">
+          {
+            this.state.classes.map( (c, i) =>
+              <div className="content1" key={i}> 
+                <h4>
+                  {c.name}
+                </h4>
+                <button onClick={this.vote.bind(this, i)}>
+                  Likes: {c.votes}
+                </button>
+              </div>
+            )
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
