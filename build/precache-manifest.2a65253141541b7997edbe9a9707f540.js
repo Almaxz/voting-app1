7e1d5e67b20a09db8a248b53a8a71d4c0acb01d2
Leo Zheng <ting5zheng@gmail.com>
2019-06-12 17:32:36 -0400

    secondary

---
+++ build/precache-manifest.2a65253141541b7997edbe9a9707f540.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e86c5ab1b020ef91e10251c935dc73e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0a41bb843f923a6afd3",
+    "url": "/static/css/main.f3ec4c15.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "d0a41bb843f923a6afd3",
+    "url": "/static/js/main.b7da810d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
